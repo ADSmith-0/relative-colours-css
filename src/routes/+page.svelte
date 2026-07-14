2c2629f23083c27dfc25f9cad312d3106307b49b
ADSmith-0 <adam00199382818@gmail.com>
2026-07-14 01:01:02 +0100

    Init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<main>
+	<h1 data-text="primary">Welcome to SvelteKit</h1>
+	<h2 data-text="secondary">Subtitle</h2>
+	<p data-text="tertiary">
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+	<button data-variant="primary">Primary</button>
+	<button data-variant="primary" data-style="outline">Primary</button>
+	<button data-variant="secondary">Secondary</button>
+	<button data-variant="secondary" data-style="outline">Secondary Outline</button>
+	<button data-variant="destructive">Delete</button>
+	<button data-variant="destructive" data-style="outline">Delete Outline</button>
+</main>
