f72f8ad6b702f42b8af1e728d49c2e5f9e18c7ca
ADSmith-0 <adam00199382818@gmail.com>
2026-08-20 23:16:08 +0100

    Updated colours

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { resolve } from '$app/paths';
 
 	let { children } = $props();
 </script>
@@ -8,4 +9,31 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<nav>
+	<a href={resolve('/oklch')}>oklch</a>
+	<a href={resolve('/relative-colours')}>Relative colours</a>
+	<a href={resolve('/colour-contrast')}>Contrast colour</a>
+	<a href={resolve('/colour-mix')}>Colour mix</a>
+</nav>
+
 {@render children()}
+
+<style>
+	nav {
+		--_bg: oklch(from var(--accent) l c h);
+		background-color: var(--_bg);
+		padding: 1rem 2rem;
+		display: flex;
+		flex-direction: row;
+		gap: 2rem;
+
+		a,
+		a:visited {
+			text-decoration: none;
+		}
+
+		a:is(:hover, :focus) {
+			color: oklch(from var(--text-primary) l c h / 0.8);
+		}
+	}
+</style>
