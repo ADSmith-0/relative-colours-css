4ab25b7d2e8e7e1cc06d2d5f8afbe44ee1e7e0d7
ADSmith-0 <adam00199382818@gmail.com>
2026-08-21 09:33:19 +0100

    Updated colour contrast

--- src/routes/color-mix/+page.svelte
+++ src/routes/color-mix/+page.svelte
@@ -1,1 +1,49 @@
+<script lang="ts">
+	let luminosity = $state<number>(0);
+	let chroma = $state<number>(0);
+	let hue = $state<number>(0);
+	let alpha = $state<number>(1);
+</script>
+
 <h1>Colour mixing</h1>
+<p>A colour scheme that is closer to how humans perceive colour</p>
+<code>color: oklch(0.5 0.2 10 / 0.9); </code>
+<div class="flex flex-row-center" style="padding-top: 1rem; gap: 2rem;">
+	<section
+		class="box"
+		style="background-color: oklch({luminosity} {chroma} {hue} / {alpha})"
+	></section>
+	<section class="flex flex-col">
+		<label for="l">Luminosity (l)</label>
+		<input id="l" type="range" min="0" max="1" step="0.05" bind:value={luminosity} />
+		<label for="c">Chroma (c)</label>
+		<input id="c" type="range" min="0" max="1" step="0.05" bind:value={chroma} />
+		<label for="h">Hue (h)</label>
+		<input id="h" type="range" min="0" max="360" step="5" bind:value={hue} />
+		<label for="a">Alpha (a)</label>
+		<input id="a" type="range" min="0" max="1" step="0.05" bind:value={alpha} />
+	</section>
+	<section>
+		<span>Hue circle:</span>
+		<div class="hue-circle"></div>
+	</section>
+</div>
+
+<style>
+	.box {
+		--size: 150px;
+		width: var(--size);
+		height: var(--size);
+	}
+
+	input {
+		margin-bottom: 1rem;
+	}
+
+	.hue-circle {
+		height: 150px;
+		width: 150px;
+		background: conic-gradient(from 0deg, red, orange, yellow, green, blue, indigo, violet, red);
+		border-radius: 50vw;
+	}
+</style>
--- src/routes/contrast-color/+page.svelte
+++ src/routes/contrast-color/+page.svelte
@@ -1,24 +1,58 @@
 <script lang="ts">
-	let bg = $state<string>("#000000");
+	let luminosity = $state<number>(0);
+	let chroma = $state<number>(0);
+	let hue = $state<number>(0);
+	let alpha = $state<number>(1);
 </script>
 
 <h1>Colour contrast</h1>
 
 <code>color: contrast-color(var(--bg));</code>
 
-<section style="--btn-bg: {bg}">
-	<div class="flex-col" style="padding-top: 2rem;">
-		<label for="color">Pick a colour, any colour!</label>
-		<input id="color" type="color" bind:value={bg} />
+<p>
+	A function in CSS that will return either black or white based on whichever gives the strongest
+	contrast
+</p>
+
+<section style="--btn-bg: oklch({luminosity} {chroma} {hue} / {alpha});">
+	<div class="flex flex-row-center" style="padding-top: 1rem; gap: 2rem;">
+		<section class="flex flex-col">
+			<label for="l">Luminosity (l)</label>
+			<input id="l" type="range" min="0" max="1" step="0.05" bind:value={luminosity} />
+			<label for="c">Chroma (c)</label>
+			<input id="c" type="range" min="0" max="1" step="0.05" bind:value={chroma} />
+			<label for="h">Hue (h)</label>
+			<input id="h" type="range" min="0" max="360" step="5" bind:value={hue} />
+			<label for="a">Alpha (a)</label>
+			<input id="a" type="range" min="0" max="1" step="0.05" bind:value={alpha} />
+		</section>
+		<section>
+			<span>Hue circle:</span>
+			<div class="hue-circle"></div>
+		</section>
 	</div>
 
-	<button>Still readable! :)</button>
+	<section class="box">Still readable! :)</section>
 </section>
 
 <style>
-	button {
+	.box {
 		margin-top: 2rem;
 		background-color: oklch(from var(--btn-bg) l c h);
 		color: contrast-color(var(--btn-bg));
+		max-width: max-content;
+		padding: 2rem;
+		border-radius: 5px;
+	}
+
+	input {
+		margin-bottom: 1rem;
+	}
+
+	.hue-circle {
+		height: 150px;
+		width: 150px;
+		background: conic-gradient(from 0deg, red, orange, yellow, green, blue, indigo, violet, red);
+		border-radius: 50vw;
 	}
 </style>
