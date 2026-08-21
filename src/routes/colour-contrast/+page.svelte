a4ada132884f2a18fc387a1f39d27e2242a96fef
ADSmith-0 <adam00199382818@gmail.com>
2026-08-21 01:17:31 +0100

    Added contrast colour

---
+++ src/routes/colour-contrast/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let bg = $state<string>("#000000");
+</script>
+
+<h1>Colour contrast</h1>
+
+<code>color: contrast-color(var(--bg));</code>
+
+<section style="--btn-bg: {bg}">
+	<div class="flex-col" style="padding-top: 2rem;">
+		<label for="color">Pick a colour, any colour!</label>
+		<input id="color" type="color" bind:value={bg} />
+	</div>
+
+	<button>Still readable! :)</button>
+</section>
+
+<style>
+	button {
+		margin-top: 2rem;
+		background-color: oklch(from var(--btn-bg) l c h);
+		color: contrast-color(var(--btn-bg));
+	}
+</style>
