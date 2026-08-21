be092636e8ab7ecdc1fab224c8dd4ef3f644afc5
ADSmith-0 <adam00199382818@gmail.com>
2026-08-21 09:48:46 +0100

    Changed styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav>
+<nav class="flex flex-row">
 	<a href={resolve('/oklch')}>oklch</a>
 	<a href={resolve('/relative-colours')}>Relative colours</a>
 	<a href={resolve('/contrast-color')}>contrast-colour</a>
@@ -25,17 +25,23 @@
 		--_bg: oklch(from var(--accent) l c h);
 		background-color: var(--_bg);
 		padding: 1rem 2rem;
-		display: flex;
-		flex-direction: row;
 		gap: 2rem;
 
 		a,
 		a:visited {
 			text-decoration: none;
+			text-decoration-color: transparent;
+			transition:
+				text-decoration,
+				text-underline-offset,
+				text-decoration-color 200ms ease-in-out;
 		}
 
 		a:is(:hover, :focus) {
 			color: oklch(from var(--text-primary) l c h / 0.8);
+			text-decoration: underline 3px;
+			text-underline-offset: 7px;
+			text-decoration-color: inherit;
 		}
 	}
 
